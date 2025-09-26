976411950df9493c04056fde21483a088fae516f
Andrew Lemons <github@aglemons.com>
2025-09-26 02:58:10 +0000

    Add env values for auth system

--- packages/env/src/schema.ts
+++ packages/env/src/schema.ts
@@ -1,3 +1,4 @@
+import crypto from "node:crypto";
 import z from "zod";
 
 export const ZEnvSchema = z.object({
@@ -6,6 +7,14 @@ export const ZEnvSchema = z.object({
 		.default("development"),
 	PORT: z.coerce.number().default(3000),
 	DATABASE_URL: z.url(),
+	AUTH_SECRET: z
+		.string()
+		.default(crypto.randomBytes(64).toString("hex"))
+		.transform((val) => {
+			const buffer = Buffer.from(val, "hex");
+			return new Uint8Array(buffer);
+		}),
+	AUTH_CAS_SERVER: z.url(),
 });
 
 export type TEnvSchema = z.infer<typeof ZEnvSchema>;
